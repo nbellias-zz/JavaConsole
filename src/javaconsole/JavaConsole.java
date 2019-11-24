544a2982b1ed0e2f8474bbd8ad6355621a4fefad
nikolaos <nikolaos@mbp-nikolaos>
2019-11-24 22:35:38 +0200

    Added some more code in main()

--- src/javaconsole/JavaConsole.java
+++ src/javaconsole/JavaConsole.java
@@ -16,6 +16,7 @@ public class JavaConsole {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("This is a console program");
     }
     
 }
